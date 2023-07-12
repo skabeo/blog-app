8397442545a596392e2f1f94f0d88f6867760761
skabeo <spencerokyere95@gmail.com>
2023-07-12 02:02:15 +0000

    Add comment model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_12_015512) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_12_015819) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.bigint "author_id", null: false
+    t.bigint "post_id", null: false
+    t.text "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["author_id"], name: "index_comments_on_author_id"
+    t.index ["post_id"], name: "index_comments_on_post_id"
+  end
 
   create_table "likes", force: :cascade do |t|
     t.bigint "author_id", null: false
@@ -43,6 +53,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_12_015512) do
     t.integer "posts_counter"
   end
 
+  add_foreign_key "comments", "posts"
+  add_foreign_key "comments", "users", column: "author_id"
   add_foreign_key "likes", "posts"
   add_foreign_key "likes", "users", column: "author_id"
   add_foreign_key "posts", "users", column: "author_id"
