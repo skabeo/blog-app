077746a6dc24554e3436bfc38288541d18180461
skabeo <spencerokyere95@gmail.com>
2023-07-12 01:57:57 +0000

    Add like model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_12_015040) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_12_015512) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "likes", force: :cascade do |t|
+    t.bigint "author_id", null: false
+    t.bigint "post_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["author_id"], name: "index_likes_on_author_id"
+    t.index ["post_id"], name: "index_likes_on_post_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.bigint "author_id", null: false
@@ -34,5 +43,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_12_015040) do
     t.integer "posts_counter"
   end
 
+  add_foreign_key "likes", "posts"
+  add_foreign_key "likes", "users", column: "author_id"
   add_foreign_key "posts", "users", column: "author_id"
 end
