8228017ec3741cf6db6ff457b0908adde7569adf
skabeo <spencerokyere95@gmail.com>
2023-07-13 22:55:56 +0000

    Fix linters errors

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe Like, type: :model do
   @user = User.new(name: 'Bob', photo: 'My cool photo', bio: 'Passionate programmer')
   @first_post = Post.new(author: @user, title: 'Culture', text: 'How do you like your chicken?')
-  subject { Like.new(author: @user, post: @first_post)}
+  subject { Like.new(author: @user, post: @first_post) }
 
   before { subject.save }
 
