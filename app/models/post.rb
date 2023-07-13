17b3f79e9044eb18e5e507a93988543184053d9d
skabeo <spencerokyere95@gmail.com>
2023-07-13 22:21:25 +0000

    Modify post model validation

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,7 +4,7 @@ class Post < ApplicationRecord
   has_many :likes, foreign_key: :post_id
 
   validates :title, presence: true, length: { maximum: 250 }
-  validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :comment_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   def update_post_counter
