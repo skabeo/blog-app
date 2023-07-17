3c46a51855530ddf065c34b3089cfac71c233426
skabeo <spencerokyere95@gmail.com>
2023-07-17 12:14:59 +0000

    Add routes to application

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get "users", to: "users#index"
+  get "users/:id", to: "users#show"
+  get "users/:user_id/posts", to: "posts#index"
+  get "users/:user_id/posts/:id", to: "posts#show"
 end
