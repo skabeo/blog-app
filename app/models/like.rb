792d4fb94d83494a5b91146cd036675ed79bd8d0
skabeo <spencerokyere95@gmail.com>
2023-07-20 19:16:56 +0000

    Modify likes counter

--- app/models/like.rb
+++ app/models/like.rb
@@ -2,7 +2,9 @@ class Like < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :post
 
-  def update_likes_counter
-    post.update(likes_counter: post.likes.count)
+  after_create :increment_likes_comment_counter
+
+  def increment_likes_comment_counter
+    post.increment!(:likes_counter)
   end
 end
