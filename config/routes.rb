497cc485ca818a05363175bd16cf4e6cb15b3d41
skabeo <spencerokyere95@gmail.com>
2023-07-25 09:51:14 +0000

    Configure capybara enviroment

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,10 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root "users#index"
   get "users", to: "users#index"
-  get "users/:id", to: "users#show"
-  get "users/:user_id/posts", to: "posts#index"
+  get "users/:id", to: "users#show", as: "user_show"
+  get "users/:user_id/posts", to: "posts#index", as: "user_post"
   get "users/:user_id/posts/new", to: "posts#new", as: "user_posts_new"
   get "users/:user_id/posts/:id", to: "posts#show", as: "user_post_details"
   post "users/:user_id/posts/create", to: "posts#create", as: "user_posts"
