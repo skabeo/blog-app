3db9d26419b51a7cc16b79d8888ea2b0d49faa2a
skabeo <spencerokyere95@gmail.com>
2023-07-26 12:40:52 +0000

    Fix linters error

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,7 +13,7 @@ class User < ApplicationRecord
 
   validates :name, presence: true
   validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
-  
+
   def set_default
     self.name = email.split('@')[0]
     self.posts_counter = 0
