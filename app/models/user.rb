e2f24e763a7e813a180a67680a49c8943ed0a266
Ikenna Richard <oguejioforrichard@gmail.com>
2023-07-26 13:30:26 -1100

    Update user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  before_validation :set_default
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -11,6 +13,11 @@ class User < ApplicationRecord
 
   validates :name, presence: true
   validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  
+  def set_default
+    self.name = email.split('@')[0]
+    self.posts_counter = 0
+  end
 
   def current_posts
     posts.order(created_at: :desc).limit(3)
