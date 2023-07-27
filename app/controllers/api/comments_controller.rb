1370cc86f07936d14be1f49f18ed9e8daeb2c5ca
skabeo <spencerokyere95@gmail.com>
2023-07-27 12:04:52 +0000

    Add api endpoint for retrieving comments of a post

--- app/controllers/api/comments_controller.rb
+++ app/controllers/api/comments_controller.rb
@@ -1,6 +1,8 @@
 class Api::CommentsController < ApplicationController
-  user = User.find(params[:user_id])
-  post = user.posts.find(params[:post_id])
-  comments = post.comments
-  render json: comments.to_json(only: [:id, :text, :created_at, :updated_at, :comments_counter, :likes_counter])
+  def index
+    user = User.find(params[:user_id])
+    post = user.posts.find(params[:post_id])
+    comments = post.comments
+    render json: comments.to_json(only: [:id, :text, :created_at, :updated_at, :comments_counter, :likes_counter])
+  end
 end
