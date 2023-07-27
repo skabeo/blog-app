017dad226ec0af7e7c81477628f1318e798d71e5
skabeo <spencerokyere95@gmail.com>
2023-07-27 13:55:49 +0000

    Fix rubocop errors

--- app/controllers/api/comments_controller.rb
+++ app/controllers/api/comments_controller.rb
@@ -3,7 +3,7 @@ class Api::CommentsController < ApplicationController
     user = User.find(params[:user_id])
     post = user.posts.find(params[:post_id])
     comments = post.comments
-    render json: comments.to_json(only: [:id, :text, :created_at, :updated_at, :comments_counter, :likes_counter])
+    render json: comments.to_json(only: %i[id text created_at updated_at comments_counter likes_counter])
   end
 
   def create
