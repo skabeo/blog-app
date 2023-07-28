50c054fbde98ef274de8a81c51496e115aa7bc61
Ikenna Richard <oguejioforrichard@gmail.com>
2023-07-27 14:47:15 -1100

    Add rack cors config

--- config/application.rb
+++ config/application.rb
@@ -20,5 +20,12 @@ module BlogApp
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    # line 23
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins 'http://127.0.0.1:5500'
+        resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
+      end
+    end
   end
 end
