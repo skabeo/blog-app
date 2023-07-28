ff1aa5236e4368f01b5aacd3a5af1af99f974aa5
Ikenna Richard <oguejioforrichard@gmail.com>
2023-07-27 14:48:19 -1100

    Update create comment method

--- app/controllers/api/comments_controller.rb
+++ app/controllers/api/comments_controller.rb
@@ -5,4 +5,22 @@ class Api::CommentsController < ApplicationController
     comments = post.comments
     render json: comments.to_json(only: [:id, :text, :created_at, :updated_at, :comments_counter, :likes_counter])
   end
+
+  def create
+    post = Post.find(params[:post_id])
+    comment = post.comments.build(comment_params)
+    comment.author_id = current_user.id
+
+    if comment.save
+      render json: comment, status: :created
+    else
+      render json: { error: comment.errors.full_messages.join(', ') }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:text)
+  end
 end
